c6638594f53b8606a228b1340a0296d64251f6d7
plinnionasc <142547568+plinnionasc@users.noreply.github.com>
2023-08-24 21:00:44 -0300

    Add files via upload

---
+++ exercicio_1/src/Vogais.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContadorDeVogais {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma frase: ");
+        String frase = scanner.nextLine();
+        scanner.close();
+
+        int contadorVogais = contarVogais(frase);
+        System.out.println("Número de vogais na frase: " + contadorVogais);
+    }
+
+    public static int contarVogais(String texto) {
+        int contador = 0;
+        for (int i = 0; i < texto.length(); i++) {
+            char c = Character.toLowerCase(texto.charAt(i));
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
