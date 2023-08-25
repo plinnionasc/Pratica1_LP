02da0f286d325aa9d2bd32c940a0d3864a4188b8
plinnionasc <142547568+plinnionasc@users.noreply.github.com>
2023-08-24 21:41:22 -0300

    Add files via upload

--- exercicio_1/src/Carro.java
+++ exercicio_1/src/Carro.java
@@ -13,12 +13,22 @@ public class Carro {
 
         if (ano<=2010)
             System.out.println("Carro velho");
+
         else if (ano<=2022)
             System.out.println("carro seminovo");
+
         else if (ano==2023)
             System.out.println("carro novo");
         else
             System.out.println("invalido");
+
+        placa= placa.toUpperCase();
+
+        for(int i=0;i<placa.length();i++){
+            if (placa.charAt(i)=='A'|| placa.charAt(i)=='E' || placa.charAt(i)=='I' || placa.charAt(i)=='O' || placa.charAt(i)=='U')
+                placa=placa.replace(placa.charAt(i),'*');
+        }
+        System.out.println("placa alterada"+placa);
         }
 
 
